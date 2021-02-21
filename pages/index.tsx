83d8a7aad0f4da035ce425f7f5649ed52290c23e
raksitnongbua <tan.raksit@gmail.com>
2021-02-21 16:05:47 +0700

    set up project from js to tsx

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout>
+      <div>Home</div>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
