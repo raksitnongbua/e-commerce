fac4d0e653c28573be080358f66fe3ad6995c5dd
raksitnongbua <tan.raksit@gmail.com>
2021-02-21 17:14:19 +0700

    add material ui

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,35 @@
+import { Button, Typography } from '@material-ui/core';
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
   return (
-    <div className='bg-red-100 flex justify-between'>
-      <div className='mx-1'>LOGO</div>
-      <div>
-        <button className='mx-1'>เข้าสู่ระบบ</button>
-        <button className='mx-1'>สมัครสมาชิก</button>
+    <div>
+      <div className='bg-blue-500 flex justify-between items-center'>
+        <div className='mx-1 p-1'>
+          <Typography>LOGO</Typography>
+        </div>
+        <div>
+          <Button variant='text' className='mx-1'>
+            <p className='text-white'>เข้าสู่ระบบ</p>
+          </Button>
+          <Button variant='text' className='mx-1'>
+            <p className='text-white'>สมัครสมาชิก</p>
+          </Button>
+        </div>
+      </div>
+      <div className='bg-blue-900 h-14 flex items-center'>
+        <div className='container mx-auto'>
+          <Link href='/shop'>
+            <Button variant='text'>ร้านค้า</Button>
+          </Link>
+          <Link href='/about'>
+            <Button variant='text'>เกี่ยวกับ</Button>
+          </Link>
+          <Link href='/contact'>
+            <Button variant='text'>ติดต่อเรา</Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
